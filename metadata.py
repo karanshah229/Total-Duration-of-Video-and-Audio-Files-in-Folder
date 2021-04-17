19f0b76e6b35ab318913c5e7e3fc1e1fbaa2b5d8
Karan Shah <karanshah229@gmail.com>
2021-04-17 16:05:47 +0530

    Add Context Menu for Each file, Fix Bug, Remove Styling

--- metadata.py
+++ metadata.py
@@ -3,36 +3,37 @@ import os
 import math
 import msvcrt
 import sys
+import time
 
 # Terminal color definitions
 
-class fg:
-    BLACK   = '\033[30m'
-    RED     = '\033[31m'
-    GREEN   = '\033[32m'
-    YELLOW  = '\033[33m'
-    BLUE    = '\033[34m'
-    MAGENTA = '\033[35m'
-    CYAN    = '\033[36m'
-    WHITE   = '\033[37m'
-    RESET   = '\033[39m'
+# class fg:
+#     BLACK   = '\033[30m'
+#     RED     = '\033[31m'
+#     GREEN   = '\033[32m'
+#     YELLOW  = '\033[33m'
+#     BLUE    = '\033[34m'
+#     MAGENTA = '\033[35m'
+#     CYAN    = '\033[36m'
+#     WHITE   = '\033[37m'
+#     RESET   = '\033[39m'
 
-class bg:
-    BLACK   = '\033[40m'
-    RED     = '\033[41m'
-    GREEN   = '\033[42m'
-    YELLOW  = '\033[43m'
-    BLUE    = '\033[44m'
-    MAGENTA = '\033[45m'
-    CYAN    = '\033[46m'
-    WHITE   = '\033[47m'
-    RESET   = '\033[49m'
+# class bg:
+#     BLACK   = '\033[40m'
+#     RED     = '\033[41m'
+#     GREEN   = '\033[42m'
+#     YELLOW  = '\033[43m'
+#     BLUE    = '\033[44m'
+#     MAGENTA = '\033[45m'
+#     CYAN    = '\033[46m'
+#     WHITE   = '\033[47m'
+#     RESET   = '\033[49m'
 
-class style:
-    BRIGHT    = '\033[1m'
-    DIM       = '\033[2m'
-    NORMAL    = '\033[22m'
-    RESET_ALL = '\033[0m'
+# class style:
+#     BRIGHT    = '\033[1m'
+#     DIM       = '\033[2m'
+#     NORMAL    = '\033[22m'
+#     RESET_ALL = '\033[0m'
 
 def only_video_audio_files(file):
     valid_extensions = [".mp4", ".mp3", ".m4a", ".m3a",
@@ -88,11 +89,16 @@ def divide(duration, speed):
 
     return ":".join(map(str, og))
 
-files = os.listdir()
+print(sys.argv[1:len(sys.argv)])
+if(len(sys.argv) > 1):
+    files = sys.argv[1:len(sys.argv)]
+else:
+    files = os.listdir()
 files = list(filter(only_video_audio_files, files))
 
 cwd = os.getcwd().split("\\")[-1]
-print(style.BRIGHT + fg.CYAN + cwd.title() + ":" + fg.RESET + style.RESET_ALL, sep="")
+# print(style.BRIGHT + fg.CYAN + cwd.title() + ":" + fg.RESET + style.RESET_ALL, sep="")
+print(cwd.title(), ":", sep="")
 individual_file_duration = {}
 total_duration = "00:00"
 if(len(files) > 0):
@@ -112,15 +118,25 @@ else:
     print("No audio / video files found")
     exit()
 
-print("Total Duration:", style.BRIGHT, total_duration, style.RESET_ALL)
-print("No of files:", style.BRIGHT, len(files), style.RESET_ALL)
-print("Total Duration" + fg.GREEN, "(1.25x): "  + fg.RESET + fg.CYAN + divide(total_duration, 1.25) + fg.RESET)
-print("Total Duration" + fg.GREEN, "(1.5x): "  + fg.RESET + fg.CYAN + divide(total_duration, 1.5) + fg.RESET)
-print("Total Duration" + fg.GREEN, "(1.75x): " + fg.RESET + fg.CYAN + divide(total_duration, 1.75) + fg.RESET)
-print("Total Duration" + fg.GREEN, "(2x): " + fg.RESET + fg.CYAN + divide(total_duration, 2) + fg.RESET)
+print("Total Duration:", total_duration)
+print("No of files:", len(files))
+print("Total Duration (1.25x):", divide(total_duration, 1.25))
+print("Total Duration (1.5x):", divide(total_duration, 1.5))
+print("Total Duration (1.75x):", divide(total_duration, 1.75))
+print("Total Duration (2x):", divide(total_duration, 2))
 print("")
 print("Enter 1 to get individual file duration")
 print("Enter any other character to quit")
+
+# print("Total Duration:", style.BRIGHT, total_duration, style.RESET_ALL)
+# print("No of files:", style.BRIGHT, len(files), style.RESET_ALL)
+# print("Total Duration" + fg.GREEN, "(1.25x): "  + fg.RESET + fg.CYAN + divide(total_duration, 1.25) + fg.RESET)
+# print("Total Duration" + fg.GREEN, "(1.5x): "  + fg.RESET + fg.CYAN + divide(total_duration, 1.5) + fg.RESET)
+# print("Total Duration" + fg.GREEN, "(1.75x): " + fg.RESET + fg.CYAN + divide(total_duration, 1.75) + fg.RESET)
+# print("Total Duration" + fg.GREEN, "(2x): " + fg.RESET + fg.CYAN + divide(total_duration, 2) + fg.RESET)
+# print("")
+# print("Enter 1 to get individual file duration")
+# print("Enter any other character to quit")
 
 # 2. Get Duration for selected items
 
@@ -129,6 +145,6 @@ try:
     if(x == 1):
         for x in individual_file_duration:
             print(x, " => ", individual_file_duration[x])
-    else: print(False)
+    msvcrt.getch()
 except Exception as e:
     exit()
