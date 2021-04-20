b8be57bb9bf45f3dae44c36f4b89b7942553b9fd
Karan Shah <karanshah229@gmail.com>
2021-04-20 11:43:26 +0530

    Updated Logic to calculate Total Duration and Bug Fixes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Calculate Total Time of all Video and Audio Files in Folder
+
+pip install -r requirements.txt
--- metadata.py
+++ metadata.py
@@ -35,6 +35,7 @@ import time
 #     NORMAL    = '\033[22m'
 #     RESET_ALL = '\033[0m'
 
+
 def only_video_audio_files(file):
     valid_extensions = [".mp4", ".mp3", ".m4a", ".m3a",
                         ".avi", ".flv", ".aac", ".ogg", ".wav", ".wma", ".raw", ".mov", ".webm", ".mpg", ".mpeg", ".3gp", ".mkv"]
@@ -44,50 +45,45 @@ def only_video_audio_files(file):
     return False
 
 
-def add_durations(orignal, dur):
-    # Split on :
-    og = orignal.split(":")
-    new = dur.split(":")
+def print_duration(duration_in_secs):
+    if(duration_in_secs < 3600):
+        # MM:SS
+        minutes = int(duration_in_secs // 60)
+        seconds = int(math.floor(duration_in_secs - (minutes*60)))
+        # Format the numbers correctly
+        if(minutes < 10): minutes = str("0" + str(minutes))
+        if(seconds < 10): seconds = str("0" + str(seconds))
+        duration = str(minutes) + ":" + str(seconds)
+    elif (duration_in_secs < 86400):
+        # HH:MM:SS
+        hours = int(duration_in_secs // 3600)
+        minutes = int(math.floor(duration_in_secs - (hours * 3600)) // 60)
+        seconds = int(math.floor(duration_in_secs - (hours*3600) - (minutes*60)))
+        # Format the numbers correctly
+        if(hours < 10): hours = str("0" + str(hours))
+        if(minutes < 10): minutes = str("0" + str(minutes))
+        if(seconds < 10): seconds = str("0" + str(seconds))
+        duration = str(hours) + ":" + str(minutes) + ":" + str(seconds)
+    elif (duration_in_secs < 2592000):
+        # DD:HH:MM:SS
+        days = int(duration_in_secs // 86400)
+        hours = int(math.floor(duration_in_secs - (days * 86400)) // 3600)
+        minutes = int(math.floor(duration_in_secs - (days*86400) - (hours*3600)) // 60)
+        seconds = int(math.floor(duration_in_secs - (days*86400) - (hours*3600) - (minutes*60)))
+        # Format the numbers correctly
+        if(days < 10): days = str("0" + str(days))
+        if(hours < 10): hours = str("0" + str(hours))
+        if(minutes < 10): minutes = str("0" + str(minutes))
+        if(seconds < 10): seconds = str("0" + str(seconds))
+        duration = str(days) + ":" + str(hours) + ":" + str(minutes) + ":" + str(seconds)
 
-    # Convert all strings to numbers
-    for i in range(len(og)):
-        og[i] = (int)(og[i])
-    for i in range(len(new)):
-        new[i] = (int)(new[i])
+    return duration
 
-    size = min(len(new), len(og))
-    carry = 0
-    res = []
 
-    for i in range(size-1, -1, -1):
-        res.insert(0, og[i] + new[i] + carry)
-        if res[0] >= 60:
-            res[0] %= 60
-            carry = 1
-            if i == 0:
-                res.insert(0, 1)
-        else: carry = 0
+def divide(duration_in_secs, speed):
+    duration_in_secs /= speed
+    return print_duration(duration_in_secs)
 
-    if(len(new) > len(og)):
-        for i in range(len(new)-1-size, -1, -1):
-            res.insert(0, new[i])
-    else:
-        for i in range(len(og)-1-size, -1, -1):
-            res.insert(0, og[i])
-
-    return ":".join(map(str, res))
-
-def divide(duration, speed):
-    og = duration.split(":")
-
-    for i in range(len(og)):
-        og[i] = (int)(og[i])
-
-    for i in range(len(og)):
-        og[i] /= speed
-        og[i] = (int)(og[i])
-
-    return ":".join(map(str, og))
 
 if(len(sys.argv) > 1):
     files = sys.argv[1:len(sys.argv)]
@@ -96,44 +92,38 @@ else:
 files = list(filter(only_video_audio_files, files))
 
 cwd = os.getcwd().split("\\")[-1]
+print("\n" + cwd.title(), ":", sep="")
 # print(style.BRIGHT + fg.CYAN + cwd.title() + ":" + fg.RESET + style.RESET_ALL, sep="")
 individual_file_duration = {}
-total_duration = "00:00"
+total_duration_in_secs = 0
 if(len(files) > 0):
-    print(cwd.title(), ":", sep="")
     for file in files:
         metadata = TinyTag.get(file)
-        duration = ""
-        if(metadata.duration < 3600):
-            # MM:SS
-            duration = str( str(math.floor(metadata.duration / 60)) + ":" + str((math.floor(metadata.duration) % 60)) )
-        elif (metadata.duration < 86400):
-            # HH:MM:SS
-            duration = str( str(math.floor(metadata.duration / 3600)) + ":" + str(math.floor(metadata.duration / 60)) + ":" + str((math.floor(metadata.duration) % 60)) )
-        
-        individual_file_duration[file] = duration
-        total_duration = add_durations(total_duration, duration)
-    
-    print("Total Duration:", total_duration)
+        individual_file_duration[file] = print_duration(math.floor(metadata.duration))
+        total_duration_in_secs += metadata.duration
+
+    total_duration_in_secs_str = print_duration(total_duration_in_secs)
+    print("Total Duration:", total_duration_in_secs_str)
     print("No of files:", len(files))
-    print("Total Duration (1.25x):", divide(total_duration, 1.25))
-    print("Total Duration (1.5x):", divide(total_duration, 1.5))
-    print("Total Duration (1.75x):", divide(total_duration, 1.75))
-    print("Total Duration (2x):", divide(total_duration, 2))
+    print("Total Duration (1.25x):", divide(total_duration_in_secs, 1.25))
+    print("Total Duration (1.5x):", divide(total_duration_in_secs, 1.5))
+    print("Total Duration (1.75x):", divide(total_duration_in_secs, 1.75))
+    print("Total Duration (2x):", divide(total_duration_in_secs, 2))
     print("")
     print("Enter 1 to get individual file duration")
     print("Enter any other character to quit")
 else:
     print("No audio / video files found")
+    print("Enter any character to quit")
     msvcrt.getch()
 
 # Specially formatted Console Outputs
-# print("Total Duration:", style.BRIGHT, total_duration, style.RESET_ALL)
+# print("Total Duration:", style.BRIGHT, total_duration_in_secs_str, style.RESET_ALL)
 # print("No of files:", style.BRIGHT, len(files), style.RESET_ALL)
-# print("Total Duration" + fg.GREEN, "(1.25x): "  + fg.RESET + fg.CYAN + divide(total_duration, 1.25) + fg.RESET)
-# print("Total Duration" + fg.GREEN, "(1.5x): "  + fg.RESET + fg.CYAN + divide(total_duration, 1.5) + fg.RESET)
-# print("Total Duration" + fg.GREEN, "(1.75x): " + fg.RESET + fg.CYAN + divide(total_duration, 1.75) + fg.RESET)
-# print("Total Duration" + fg.GREEN, "(2x): " + fg.RESET + fg.CYAN + divide(total_duration, 2) + fg.RESET)
+# print("Total Duration" + fg.GREEN, "(1.25x): "  + fg.RESET + fg.CYAN + divide(total_duration_in_secs, 1.25) + fg.RESET)
+# print("Total Duration" + fg.GREEN, "(1.5x): "  + fg.RESET + fg.CYAN + divide(total_duration_in_secs, 1.5) + fg.RESET)
+# print("Total Duration" + fg.GREEN, "(1.75x): " + fg.RESET + fg.CYAN + divide(total_duration_in_secs, 1.75) + fg.RESET)
+# print("Total Duration" + fg.GREEN, "(2x): " + fg.RESET + fg.CYAN + divide(total_duration_in_secs, 2) + fg.RESET)
 # print("")
 # print("Enter 1 to get individual file duration")
 # print("Enter any other character to quit")
@@ -143,8 +133,10 @@ else:
 try:
     x = int(input())
     if(x == 1):
+        print()
         for x in individual_file_duration:
             print(x, " => ", individual_file_duration[x])
+    print("\nEnter any character to quit")
     msvcrt.getch()
 except Exception as e:
     exit()
